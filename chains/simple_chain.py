1dd3c0d5d0ac6fe1b0ab4aeb359a8873116d6106
AnujBhimaniya <bhimaniyaanuj02@gmail.com>
2026-03-19 15:38:20 +0530

    learning : chains

---
+++ chains/simple_chain.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatOpenAI()
+template = PromptTemplate(template='Give me 5 amazing facts about {topic}', input_variables=['topic'])
+parser = StrOutputParser()
+
+
+chain = template | model | parser
+result = chain.invoke({'topic' : 'India'})
+# print(result)
+chain.get_graph().print_ascii()
